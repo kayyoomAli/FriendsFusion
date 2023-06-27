a25422d80d45b9baaabbb5e4d0d22ec5afdb8d52
kayyoomAli <71779491+kayyoomAli@users.noreply.github.com>
2023-06-27 13:14:43 +0530

    new customTextInput

--- src/custom/CustomTextInput.tsx
+++ src/custom/CustomTextInput.tsx
@@ -1,3 +1,121 @@
+// import {
+//   View,
+//   Text,
+//   Image,
+//   TextInput,
+//   TextStyle,
+//   ViewStyle,
+//   StyleProp,
+//   StyleSheet,
+//   ImageStyle,
+//   TouchableOpacity,
+//   KeyboardTypeOptions,
+// } from 'react-native';
+// import React from 'react';
+// import {vh, vw} from '../utils/dimensions';
+// import colors from '../utils/colors';
+// /**
+//  * @CustomTextInputProps interface
+//  * @description defining the props of TextInput
+//  */
+// interface CustomTextInputProps {
+//   ref?: any;
+//   onFocus?: any;
+//   leftIcon?: any;
+//   inputMode?: any;
+//   rightIcon?: any;
+//   leftText?: string;
+//   editable?: boolean;
+//   lefIconOnPress?: any;
+//   placeholder?: string;
+//   rightIconOnPress?: any;
+//   value?: string | undefined;
+//   placeholderTextColor?: string;
+//   maxLength?: number | undefined;
+//   mainViewStyle?: StyleProp<ViewStyle>;
+//   secureTextEntry?: boolean | undefined;
+//   leftIconStyle?: StyleProp<ImageStyle>;
+//   rightIconStyle?: StyleProp<ImageStyle>;
+//   keyboardType?: KeyboardTypeOptions | undefined;
+//   TextInputstyle?: StyleProp<TextStyle> | undefined;
+//   onChangeText?: ((text: string) => void) | undefined;
+// }
+
+// /**
+//  * @CustomTextInput function
+//  * @description custom component of TextInput
+//  */
+// const CustomTextInput = React.forwardRef(
+//   (props: CustomTextInputProps, ref: any) => {
+//     // const handleFocus = () => {
+//     //   ref?.current?.select();
+//     // };
+//     return (
+//       <View style={[styles.mainContainerStyle, props.mainViewStyle]}>
+//         {props.leftIcon && (
+//           <TouchableOpacity activeOpacity={0.7} onPress={props.lefIconOnPress}>
+//             <Image
+//               resizeMode="contain"
+//               source={props.leftIcon}
+//               style={[styles.imageStlye, props.leftIconStyle]}
+//             />
+//           </TouchableOpacity>
+//         )}
+//         {props.leftText && (
+//           <TouchableOpacity activeOpacity={0.7} onPress={props.lefIconOnPress}>
+//             <Text style={{color: colors.black}}>{props.leftText}</Text>
+//           </TouchableOpacity>
+//         )}
+//         <TextInput
+//           ref={ref}
+//           value={props.value}
+//           // onFocus={handleFocus}
+//           editable={props.editable}
+//           maxLength={props.maxLength}
+//           onChangeText={props.onChangeText}
+//           keyboardType={props.keyboardType}
+//           secureTextEntry={props.secureTextEntry}
+//           placeholder={props.placeholder || 'Placeholder'}
+//           style={[styles.inputStyle, props.TextInputstyle]}
+//           placeholderTextColor={props.placeholderTextColor || 'black'}
+//         />
+//         {props.rightIcon && (
+//           <TouchableOpacity
+//             activeOpacity={0.7}
+//             onPress={props.rightIconOnPress}>
+//             <Image
+//               resizeMode="contain"
+//               source={props.rightIcon}
+//               style={[styles.imageStlye, props.rightIconStyle]}
+//             />
+//           </TouchableOpacity>
+//         )}
+//       </View>
+//     );
+//   },
+// );
+
+// export default React.memo(CustomTextInput);
+
+// const styles = StyleSheet.create({
+//   mainContainerStyle: {
+//     borderWidth: 1,
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     marginHorizontal: vw(10),
+//     justifyContent: 'space-between',
+//   },
+//   inputStyle: {
+//     flex: 1,
+//     height: vh(50),
+//     paddingHorizontal: vw(10),
+//   },
+//   imageStlye: {
+//     height: vw(20),
+//     width: vw(20),
+//   },
+// });
+
 import {
   View,
   Text,
@@ -10,35 +128,39 @@ import {
   ImageStyle,
   TouchableOpacity,
   KeyboardTypeOptions,
+  ImageSourcePropType,
 } from 'react-native';
 import React from 'react';
-import {vh, vw} from '../utils/dimensions';
-import colors from '../utils/colors';
+import colors from '../utils/color';
+import {normalize, vh, vw} from '../utils/dimension';
+
 /**
  * @CustomTextInputProps interface
  * @description defining the props of TextInput
  */
 interface CustomTextInputProps {
   ref?: any;
-  onFocus?: any;
-  leftIcon?: any;
-  inputMode?: any;
-  rightIcon?: any;
   leftText?: string;
+  rightText?: string;
   editable?: boolean;
-  lefIconOnPress?: any;
   placeholder?: string;
-  rightIconOnPress?: any;
+  rightTextColor?: string;
   value?: string | undefined;
+  lefIconOnPress?: () => void;
+  rightIconOnPress?: () => void;
   placeholderTextColor?: string;
   maxLength?: number | undefined;
+  leftIcon?: ImageSourcePropType;
+  rightIcon?: ImageSourcePropType;
+  multiline?: boolean | undefined;
   mainViewStyle?: StyleProp<ViewStyle>;
   secureTextEntry?: boolean | undefined;
   leftIconStyle?: StyleProp<ImageStyle>;
   rightIconStyle?: StyleProp<ImageStyle>;
   keyboardType?: KeyboardTypeOptions | undefined;
-  TextInputstyle?: StyleProp<TextStyle> | undefined;
+  textInputstyle?: StyleProp<TextStyle> | undefined;
   onChangeText?: ((text: string) => void) | undefined;
+  textAlignVertical?: 'center' | 'auto' | 'bottom' | 'top' | undefined;
 }
 
 /**
@@ -47,47 +169,62 @@ interface CustomTextInputProps {
  */
 const CustomTextInput = React.forwardRef(
   (props: CustomTextInputProps, ref: any) => {
-    // const handleFocus = () => {
-    //   ref?.current?.select();
-    // };
     return (
       <View style={[styles.mainContainerStyle, props.mainViewStyle]}>
         {props.leftIcon && (
-          <TouchableOpacity activeOpacity={0.7} onPress={props.lefIconOnPress}>
+          <TouchableOpacity
+            style={styles.touchableImageStyle}
+            activeOpacity={0.7}
+            onPress={props.lefIconOnPress}>
             <Image
               resizeMode="contain"
               source={props.leftIcon}
-              style={[styles.imageStlye, props.leftIconStyle]}
+              style={[styles.imageStyle, props.leftIconStyle]}
             />
           </TouchableOpacity>
         )}
         {props.leftText && (
-          <TouchableOpacity activeOpacity={0.7} onPress={props.lefIconOnPress}>
+          <TouchableOpacity
+            style={styles.touchableImageStyle}
+            activeOpacity={0.7}
+            onPress={props.lefIconOnPress}>
             <Text style={{color: colors.black}}>{props.leftText}</Text>
           </TouchableOpacity>
         )}
         <TextInput
           ref={ref}
           value={props.value}
-          // onFocus={handleFocus}
           editable={props.editable}
           maxLength={props.maxLength}
+          multiline={props.multiline}
           onChangeText={props.onChangeText}
           keyboardType={props.keyboardType}
           secureTextEntry={props.secureTextEntry}
+          textAlignVertical={props.textAlignVertical}
           placeholder={props.placeholder || 'Placeholder'}
-          style={[styles.inputStyle, props.TextInputstyle]}
-          placeholderTextColor={props.placeholderTextColor || 'black'}
+          style={[styles.inputStyle, props.textInputstyle]}
+          placeholderTextColor={props.placeholderTextColor || 'grey'}
         />
         {props.rightIcon && (
           <TouchableOpacity
             activeOpacity={0.7}
+            style={styles.touchableRightIconStyle}
             onPress={props.rightIconOnPress}>
             <Image
               resizeMode="contain"
               source={props.rightIcon}
-              style={[styles.imageStlye, props.rightIconStyle]}
+              style={[styles.imageStyle, props.rightIconStyle]}
             />
+          </TouchableOpacity>
+        )}
+        {props.rightText && (
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.rightTextStyle}
+            onPress={props.rightIconOnPress}>
+            <Text style={{color: props.rightTextColor || colors.black}}>
+              {props.rightText}
+            </Text>
           </TouchableOpacity>
         )}
       </View>
@@ -99,19 +236,33 @@ export default React.memo(CustomTextInput);
 
 const styles = StyleSheet.create({
   mainContainerStyle: {
-    borderWidth: 1,
+    borderWidth: 0.3,
     flexDirection: 'row',
     alignItems: 'center',
-    marginHorizontal: vw(10),
+    marginHorizontal: vh(25),
     justifyContent: 'space-between',
+    backgroundColor: colors.white,
+    borderColor: colors.shade_grey,
+    borderRadius: normalize(10),
   },
   inputStyle: {
     flex: 1,
-    height: vh(50),
-    paddingHorizontal: vw(10),
+    height: vh(60),
+    paddingHorizontal: vw(15),
+    color: colors.black,
   },
-  imageStlye: {
+  imageStyle: {
     height: vw(20),
     width: vw(20),
   },
+  touchableImageStyle: {
+    paddingLeft: vw(15),
+  },
+  touchableRightIconStyle: {
+    paddingRight: vw(15),
+  },
+  rightTextStyle: {
+    marginRight: vw(10),
+  },
 });
+
